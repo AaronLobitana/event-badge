7b6a7009f700ba90e36db7eebe77533f9be8ef06
AaronLobitana <faaron.lobitana@gmail.com>
2023-04-27 13:24:33 -0400

    event-badge
    basically done, just need to edit some

--- src/badge-list.js
+++ src/badge-list.js
@@ -62,7 +62,33 @@ export class BadgeList extends LitElement{
             
             ${this.badges.map(badge => html`
             <div class="item">
-                <event-badge name="${badge.name}" line2="${badge.line2}" line3="${badge.line3}" media="${badge.media}" mainimage="${badge.mainimage}" secimage="${badge.secimage}" ?sepia="${badge.sepia}" ?bw="${badge.bw}" tvcolor="${badge.tvcolor}"></event-badge>
+                <event-badge 
+                name="${badge.name}" 
+                line2="${badge.line2}" 
+                line3="${badge.line3}" 
+                media="${badge.media}" 
+                mainimage="${badge.mainimage}" 
+                secimage="${badge.secimage}" 
+                ?sepia="${badge.sepia}" 
+                ?bw="${badge.bw}" 
+                tvcolor="${badge.tvcolor}" 
+                topcolor1="${badge.topcolor1}" 
+                topcolor2="${badge.topcolor2}" 
+                topcolor3="${badge.topcolor3}" 
+                topcolor4="${badge.topcolor4}" 
+                topcolor5="${badge.topcolor5}" 
+                topcolor6="${badge.topcolor6}" 
+                topcolor7="${badge.topcolor7}" 
+                bottomcolor1="${badge.bottomcolor1}" 
+                bottomcolor2="${badge.bottomcolor2}" 
+                bottomcolor3="${badge.bottomcolor3}" 
+                bottomcolor4="${badge.bottomcolor4}" 
+                bottomcolor5="${badge.bottomcolor5}" 
+                bottomcolor6="${badge.bottomcolor6}" 
+                bottomcolor7="${badge.bottomcolor7}" 
+                backgroundcolor="${badge.backgroundcolor}" 
+                knobscolor="${badge.knobscolor}" 
+                fontcolor="${badge.fontcolor}"></event-badge>
             </div>
             `)}
         </div>
